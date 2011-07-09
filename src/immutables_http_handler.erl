6acc4a1916e2f84583e8e6b35b9bb2d0929a7806
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-07-09 23:19:24 +0700

    Serve page using web server

--- src/immutables_http_handler.erl
+++ src/immutables_http_handler.erl
@@ -1,5 +1,25 @@
 -module(immutables_http_handler).
 -export([handle_http/1]).
 
+-include_lib("kernel/include/file.hrl").
+
 handle_http(Req) ->
-    Req:ok("Hello world").
+    handle(Req:get(method), Req:resource([urldecode]), Req).
+
+handle('GET', [], Req) ->
+    Req:file("/Users/grig/Projects/spawnfest/immutables/priv/public/index.html");
+handle('GET', Path, Req) ->
+    FullPath = "/Users/grig/Projects/spawnfest/immutables/priv/public/" ++ filename:join(Path),
+    case file:read_file_info(FullPath) of
+	{ok, #file_info{type=regular}} ->
+	    Req:file(FullPath);
+	{ok, _} ->
+	    Req:respond(404);
+	{error, enoent} ->
+	    handle_resource('GET', Path, Req)
+    end;
+handle(Method, Resource, Req) ->
+    handle_resource(Method, Resource, Req).
+
+handle_resource(_, _, Req) ->
+    Req:ok("Hello, world!").
